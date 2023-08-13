924ed9418cfbb12b88873dc848fcac9b54a0e6a1
fcoagz <grihardware@gmail.com>
2023-08-13 17:17:52 -0400

    config bot

---
+++ src/config_bot.py
@@ -0,0 +1,41 @@
+import json
+
+class Config(object):
+    token = '6634355551:AAEPLtWvLhBVQlWfIeLohOZeNu-mdnPAEqI'
+
+    @classmethod
+    def see_users(self):
+        with open("src/db.json", "r+") as f:
+            f_json: dict = json.load(f)
+
+            return len([value for value in f_json.keys()])
+    
+    @classmethod
+    def secure_user(self, chat_id: str):
+        with open("src/db.json", "r+") as f:
+            f_json: dict = json.load(f)
+
+            if chat_id not in f_json.keys():
+                return False
+            return True
+
+    @classmethod
+    def add_user(self, chat_id: str):
+        with open("src/db.json", "r+") as f:
+            f_json = json.load(f)
+
+            f_json[chat_id] = {'Ai': 'GetGpt', 'Model': 'gpt-3.5-turbo'}
+            f.seek(0)
+            f.write(json.dumps(f_json, indent = 2))
+            f.truncate()
+    
+    @classmethod
+    def setup_Ai(self, chat_id: str, Ai: str, model: str):
+        with open("src/db.json", "r+") as f:
+            f_json = json.load(f)
+
+            f_json[chat_id] = {'Ai': Ai, 'Model': model}
+            f.seek(0)
+            f.write(json.dumps(f_json, indent = 2))
+            f.truncate()
+            
